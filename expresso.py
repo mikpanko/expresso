a7f572ec5bb9def6429baa04d68ed780edaf1e0c
Mikhail Panko <mikpanko@gmail.com>
2013-12-21 17:21:08 -0500

    added initial interface and procfile

--- expresso.py
+++ expresso.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def expresso():
+    return render_template('expresso.html')
 
 
 if __name__ == '__main__':
