dfa8a992dbae159583c0f5192376d40da6c6d08b
Mikhail Panko <mikpanko@gmail.com>
2013-12-24 12:49:24 -0500

    minor change to sentence ratio calculation

--- static/js/expresso.js
+++ static/js/expresso.js
@@ -29,9 +29,9 @@ $(function(){
                     $("#word-count").text(result.word_count.toString());
                     $("#vocabulary-size").text(result.vocabulary_size.toString());
                     $("#sentence-count").text(result.sentence_count.toString());
-                    $("#declarative-ratio").text(Math.round(result.declarative_ratio).toString() + "%");
-                    $("#interrogative-ratio").text(Math.round(result.interrogative_ratio).toString() + "%");
-                    $("#exclamative-ratio").text(Math.round(result.exclamative_ratio).toString() + "%");
+                    $("#declarative-ratio").text(Math.round(result.declarative_ratio * 100).toString() + "%");
+                    $("#interrogative-ratio").text(Math.round(result.interrogative_ratio * 100).toString() + "%");
+                    $("#exclamative-ratio").text(Math.round(result.exclamative_ratio * 100).toString() + "%");
                     $("#results-table").show();
                 },
                 error: function(request, textStatus, error) {
