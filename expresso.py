bfdcf7c3f28d0010676f2dec2fff1b2890b601cc
Mikhail Panko <mikpanko@gmail.com>
2013-12-26 00:17:41 -0500

    testing why still can't connect to remote database

--- expresso.py
+++ expresso.py
@@ -5,9 +5,14 @@ from text_analysis import analyze_text
 from model import db_proxy, Text
 
 app = Flask(__name__)
+
+print app.config
+print os.environ
 app.config.update(**os.environ)
+print app.config
 
 db = MySQLDatabase(app.config['DATABASE'], host=app.config['HOST'], port=int(app.config['PORT']), user=app.config['USER'], passwd=app.config['PASSWORD'])
+#db = MySQLDatabase('heroku_a05e28ca77685c2', host='us-cdbr-east-04.cleardb.com', port=int('3306'), user='b6924537e41c0f', passwd='09f66988')
 db_proxy.initialize(db)
 db.connect()
 
