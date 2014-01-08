f2dc253aca6bbe6c4569bfa188c9430da86061a9
Mikhail Panko <mikpanko@gmail.com>
2014-01-08 00:09:03 -0500

    added loading state spinner during text analysis

--- static/js/expresso.js
+++ static/js/expresso.js
@@ -23,6 +23,28 @@ $(function(){
         // hide results table
         resultsTable.hide();
 
+        // create loading state spinner
+        var spinnerOpts = {
+            lines: 13, // The number of lines to draw
+            length: 20, // The length of each line
+            width: 10, // The line thickness
+            radius: 30, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 0, // The rotation offset
+            direction: 1, // 1: clockwise, -1: counterclockwise
+            color: '#808080', // #rgb or #rrggbb or array of colors
+            speed: 1, // Rounds per second
+            trail: 60, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner', // The CSS class to assign to the spinner
+            zIndex: 2e9, // The z-index (defaults to 2000000000)
+            top: 'auto', // Top position relative to parent in px
+            left: 'auto' // Left position relative to parent in px
+        };
+        var spinner = new Spinner(spinnerOpts);
+        $("#spinner-container").hide();
+
         // get rid of active state on the mobile menu button
         $(".navbar-toggle").click(function() {
             $(".navbar-toggle").blur();
@@ -93,6 +115,8 @@ $(function(){
                     removeMetricHighlighting(el);
                 });
                 $(".metric").removeClass("metric-active");
+                $("#spinner-container").show();
+                spinner.spin(document.getElementById("spinner-container"));
 
                 // send text to the server for analysis
                 $.ajax({
@@ -108,6 +132,8 @@ $(function(){
                         text = result.text;
                         tokens = result.tokens;
                         metrics = result.metrics;
+                        spinner.stop();
+                        $("#spinner-container").hide();
                         addMetricsToResultsTable();
                         textField.html(renderTokensToHtml());
                         resultsTable.show();
